6ed7648e51f31a9bb85aa646a362128c28285320
jniedballa <camtrapr@gmail.com>
2018-10-21 14:35:47 +0800

    no default main title

--- camtrapR/R/activityOverlap.R
+++ camtrapR/R/activityOverlap.R
@@ -15,14 +15,14 @@ activityOverlap <- function(recordTable,
                             ...){
 
 
-  wd0 <- getwd()
+  wd0  <- getwd()
   mar0 <- par()$mar
   on.exit(setwd(wd0))
   on.exit(par(mar = mar0), add = TRUE)
 
   checkForSpacesInColumnNames(speciesCol = speciesCol, recordDateTimeCol = recordDateTimeCol)
   if(!is.data.frame(recordTable)) stop("recordTable must be a data frame", call. = FALSE)
-  if(!speciesCol %in% colnames(recordTable))           stop(paste('speciesCol = "', speciesCol, '" is not a column name in recordTable', sep = ''), call. = FALSE)
+  if(!speciesCol %in% colnames(recordTable))        stop(paste('speciesCol = "', speciesCol, '" is not a column name in recordTable', sep = ''), call. = FALSE)
   if(!recordDateTimeCol %in% colnames(recordTable)) stop(paste('recordDateTimeCol = "', recordDateTimeCol, '" is not a column name in recordTable', sep = ''), call. = FALSE)
   
   stopifnot(is.logical(c(writePNG, plotR, createDir)))
@@ -47,7 +47,7 @@ activityOverlap <- function(recordTable,
   
   if(nrow(subset_speciesA) == 1) stop("speciesA has only 1 record. Cannot estimate density.")
   if(nrow(subset_speciesB) == 1) stop("speciesB has only 1 record. Cannot estimate density.")
-
+  
   if(any(is.na( c(subset_speciesA$Time.rad, subset_speciesB$Time.rad))))stop("NAs produced in converting to radial time. Sorry, that's a bug. Please report it.")
 
   n_record_string <- paste("number of records:", paste(nrow(subset_speciesA), nrow(subset_speciesB), sep = " / "))
@@ -58,18 +58,19 @@ activityOverlap <- function(recordTable,
 
   dhat.tmp <- paste(names(overlapEst.tmp)[1],
                     round(overlapEst.tmp, digits = 2)[1], sep = "=")
-  cex.sub <- 0.75
-  pngWidth <- pngMaxPix
-  pngHeight <- round(pngMaxPix * 0.8)
-  mar.tmp <-  c(7.1, 4.1, 4.1, 2.1)
+  cex.sub      <- 0.75
+  pngWidth     <- pngMaxPix
+  pngHeight    <- round(pngMaxPix * 0.8)
+  mar.tmp      <-  c(7.1, 4.1, 4.1, 2.1)
   y_usr_factor <- 0.22
 
+  # extract line type, color and width for use in legend
   dots <- list(...)
   if(!is.null(dots[['linetype']])){ lty.tmp <- dots[['linetype']]}  else {lty.tmp <- c(1, 2)}
   if(!is.null(dots[['linewidth']])){lwd.tmp <- dots[['linewidth']]} else {lwd.tmp <- c(1, 1)}
   if(!is.null(dots[['linecol']])){  col.tmp <- dots[['linecol']]}   else {col.tmp <- c("black", "blue")}
 
-
+# check that plot directory exists, create it if needed and desired, and set working directory
   if(isTRUE(writePNG)){
     if(is.null(plotDirectory) == FALSE){
       if(isTRUE(createDir)){
@@ -85,25 +86,29 @@ activityOverlap <- function(recordTable,
   if(isTRUE(writePNG)){
 
     png(filename = paste("activity_overlap_", speciesA, "-", speciesB, "_", Sys.Date(), ".png", sep = ""),
-        width = pngWidth, height = pngHeight, units = "px", res = 96, type = "cairo")
+        width    = pngWidth, 
+        height   = pngHeight, 
+        units    = "px", 
+        res      = 96, 
+        type     = "cairo")
 
     par(mar = mar.tmp)
-    overlapPlot(A = subset_speciesA$Time.rad, B = subset_speciesB$Time.rad,
-                main = paste("Activity overlap: ", speciesA, "-", speciesB),
-                rug = add.rug,
+    overlapPlot(A    = subset_speciesA$Time.rad, 
+                B    = subset_speciesB$Time.rad,
+                rug  = add.rug,
                 ...)
-    legend( x = "top",
+    legend( x      = "top",
             legend = dhat.tmp,
-            bty = "n",
-            cex = cex.sub)
+            bty    = "n",
+            cex    = cex.sub)
 
     if(isTRUE(addLegend)){
-      legend(x = legendPosition,
+      legend(x      = legendPosition,
              legend = c(speciesA, speciesB),
-             lty = lty.tmp,
-             lwd = lwd.tmp,
-             col = col.tmp,
-             bg = "white"
+             lty    = lty.tmp,
+             lwd    = lwd.tmp,
+             col    = col.tmp,
+             bg     = "white"
       )
     }
     mtext(n_record_string, side = 3, line = 0)            
@@ -114,22 +119,21 @@ activityOverlap <- function(recordTable,
 
     par(mar = mar.tmp)
     plot.values <- overlapPlot(A = subset_speciesA$Time.rad, B = subset_speciesB$Time.rad,
-                               main = paste("Activity overlap: ", speciesA, "-", speciesB),
                                rug = add.rug,
                                ... )
 
-    legend(x = "top",
+    legend(x      = "top",
            legend = dhat.tmp,
-           bty = "n",
-           cex = cex.sub)
+           bty    = "n",
+           cex    = cex.sub)
 
     if(isTRUE(addLegend)){
-      legend(x = legendPosition,
+      legend(x      = legendPosition,
              legend = c(speciesA, speciesB),
-             lty = lty.tmp,
-             lwd = lwd.tmp,
-             col = col.tmp,
-             bg = "white"
+             lty    = lty.tmp,
+             lwd    = lwd.tmp,
+             col    = col.tmp,
+             bg     = "white"
       )
     }
     mtext(n_record_string, side = 3, line = 0)            
