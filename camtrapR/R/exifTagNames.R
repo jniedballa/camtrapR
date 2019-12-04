73f9e9c7e4fc162dc08079b0f22fbaae57c82665
jniedballa <camtrapr@gmail.com>
2019-12-04 14:04:28 +0800

    support for nDir / filename combinations

--- camtrapR/R/exifTagNames.R
+++ camtrapR/R/exifTagNames.R
@@ -1,28 +1,51 @@
 exifTagNames <- function(inDir, 
-                                  whichSubDir = 1, 
-                                  returnMetadata = FALSE, 
-                                  returnTagGroup = TRUE){
-
-  stopifnot(file.exists(inDir))
+                         whichSubDir = 1, 
+                         fileName,
+                         returnMetadata = FALSE, 
+                         returnTagGroup = TRUE){
+  
+  
   stopifnot(is.logical(returnMetadata))
   stopifnot(is.logical(returnTagGroup))
   
-  if(Sys.which("exiftool") == "") stop("cannot find ExifTool")
-  if(!dir.exists(inDir)) stop("Could not find inDir:\n", inDir, call. = FALSE)
+  if(Sys.which("exiftool") == "") stop("cannot find ExifTool", call. = FALSE)
   
-  dirs.tmp <- list.dirs(inDir, recursive = FALSE, full.names = TRUE)
-
-  if(file.exists(dirs.tmp[whichSubDir]) == FALSE) stop("the specified subdirectory does not exist. Check argument 'whichSubDir'")
-
-  file.tmp <- list.files(dirs.tmp[whichSubDir],
-                         full.names = TRUE,
-                         pattern    = ".JPG$|.jpg$",
-                         recursive  = TRUE)[1]
-  if(length(file.tmp) == 0) stop(paste("found no jpg in ", dirs.tmp[whichSubDir], sep = "\n"))
-
-  message(paste("Extracted metadata of:", file.tmp))
+  if(hasArg(fileName)){
+    
+    if(!hasArg(inDir)){
+      file.tmp <- fileName
+    } else {
+      if(!dir.exists(inDir)) stop("Could not find inDir:\n", inDir, call. = FALSE)
+      file.tmp <- file.path(inDir, fileName)
+    }
+    if(!file.exists(file.tmp)) stop(paste(file.tmp, "not found"))
+    
+  } else { # if fileName is not defined
+    
+    if(!hasArg(inDir))     stop("fileName is not defined. Please define 'inDir' then", call. = FALSE)
+    if(!dir.exists(inDir)) stop("Could not find inDir:\n", inDir, call. = FALSE)
+    
+    if(is.numeric(whichSubDir)){
+      dir.tmp <- list.dirs(inDir, recursive = FALSE, full.names = TRUE)[whichSubDir]
+      if(!dir.exists(dir.tmp)) stop("the specified subdirectory does not exist. Check argument 'whichSubDir'")
+    }
+    if(is.character(whichSubDir)){
+      dir.tmp <- file.path(inDir, whichSubDir)
+    }
+    
+    file.tmp <- list.files(dir.tmp,
+                           full.names = TRUE,
+                           pattern    = ".JPG$|.jpg$",
+                           recursive  = TRUE)[1]
+    if(length(file.tmp) == 0) stop(paste("found no jpg in ", dir.tmp, sep = "\n"))
+    
+  }
   
-  if(returnMetadata == FALSE){
+  
+  if(!returnMetadata)  message(paste("Metadata of:", file.tmp, sep = "\n"))
+  if(returnMetadata)  message(paste("Metadata values of:", file.tmp, sep = "\n"))
+  
+  if(!isTRUE(returnMetadata)){
     command.tmp  <- paste('exiftool -csv', ifelse(returnTagGroup, ' -G',  ''), ' "', file.tmp, '"', sep = "")
     metadata.tmp <- system(command.tmp, intern=TRUE)
     tagnames     <- sort(unlist(strsplit(metadata.tmp[[1]], split = ",")))
