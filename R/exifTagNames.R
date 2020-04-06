c2de31a91815a38c506621b7e20e714a4380fb85
jniedballa <camtrapr@gmail.com>
2020-04-06 02:23:48 +0200

    potentially more flexible with the Group names

--- R/exifTagNames.R
+++ R/exifTagNames.R
@@ -27,12 +27,14 @@ exifTagNames <- function(inDir,
     
     if(is.numeric(whichSubDir)){
       dir.tmp <- list.dirs(inDir, recursive = FALSE, full.names = TRUE)[whichSubDir]
-      if(!dir.exists(dir.tmp)) stop("the specified subdirectory does not exist. Check argument 'whichSubDir'")
     }
     if(is.character(whichSubDir)){
       dir.tmp <- file.path(inDir, whichSubDir)
     }
+    if(!dir.exists(dir.tmp)) stop(paste("the specified subdirectory does not exist. Check argument 'whichSubDir.'\n", 
+                                        dir.tmp), call. = FALSE)
     
+    # filename of first image in specified directory
     file.tmp <- list.files(dir.tmp,
                            full.names = TRUE,
                            pattern    = ".JPG$|.jpg$",
@@ -43,16 +45,22 @@ exifTagNames <- function(inDir,
   
   message(paste("Metadata of:", file.tmp, sep = "\n"))
 
+  # generate exiftool calls
   
   # exiftool arguments: 
   # -s1: print tag  names instead of description
   # -t:  tab-delimited output
-  # -G: return Group names
+  # -G: return Group names # 0 (general location), 1 (specific location), 2 (category), 3 (document number), 4 (instance number), 5 (metadata path) or 6 (EXIF/TIFF format)
   
-  command.tmp1  <- paste('exiftool -s1 -t -G', ' "', file.tmp, '"', sep = "")
+  groupHeadings_general  <- "-G0"    # can possibly also be -G0:1 
+  #groupHeadings_specific <- "-G1"   # when using this separately from -G0, output is sorted differently (by group)
+  
+  
+  command.tmp1  <- paste('exiftool -s1 -t ', groupHeadings_general, ' "', file.tmp, '"', sep = "")
   # run without -s1 option to extract tag descriptions instead of tag names
-  command.tmp2 <- paste('exiftool -t -G', ' "', file.tmp, '"', sep = "")
+  command.tmp2 <- paste('exiftool -t ', groupHeadings_general, ' "', file.tmp, '"', sep = "")
   
+  # run exiftool
   metadata.tmp1 <- system(command.tmp1, intern=TRUE)
   metadata.tmp2 <- system(command.tmp2, intern=TRUE)
 
@@ -64,11 +72,13 @@ exifTagNames <- function(inDir,
                      col.names = c("tag_group", "tag_description", "value"), sep = "\t",
                      stringsAsFactors = FALSE, fill = TRUE)
 
-  if(any(out1$value != out2$value))  stop("Order of exiftool-extract metadata differs. Please report this bug.")
+  # make sure out1 and out2 are ordered identically
+  if(any(out1$value != out2$value))  stop("Order of exiftool-extracted metadata differs. Please report this bug.")
+  
   # combine data frames
   out <- cbind(tag_group       = out1$tag_group,
-               tag_description = out2$tag_description,
                tag_name        = out1$tag_name,
+               tag_description = out2$tag_description,
                value           = out1$value)
   return(as.data.frame(out))
 }
