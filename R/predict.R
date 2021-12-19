0a7a359a71f0b2e65199b9649e4ef1ee52fb6c7f
Jürgen Niedballa <camtrapr@gmail.com>
2021-12-19 15:28:42 +0100

    simplify, more consistent with plot_*, works for x=data.frame

--- R/predict.R
+++ R/predict.R
@@ -19,131 +19,158 @@ predictionMapsCommunity <- function(object,
                                     level = 0.95,
                                     interval = c("none", "confidence"),
                                     x,
-                                    speciesSubset,
-                                    categ_order = "species,factor") 
+                                    speciesSubset) 
 {
   
   type <- match.arg(type, choices = c("psi", "richness"))
   interval <- match.arg(interval, choices = c("none", "confidence"))
-  
+
   # subset occupancy (beta) parameters
   submodel <- "state"
-  params <- object@params[grep("^beta", object@params)]
   
-  categ_order <- match.arg(categ_order, choices = c("species,factor", "factor,species"))
+  submodel <- match.arg(submodel, choices = c("det", "state"))
   
+  if(submodel == "state") {
+    keyword_submodel <- "^beta"
+    keyword_submodel_short <- "beta"
+  }
+  if(submodel == "det") {
+    keyword_submodel <- "^alpha"
+    keyword_submodel_short <- "alpha"
+  } 
   
+  # get covariate information for submodel
   cov_info_subset <- object@covariate_info[object@covariate_info$submodel == submodel & object@covariate_info$param == "param",]
+  
   if(nrow(cov_info_subset) == 0) stop(paste("No covariates in submodel", submodel), call. = F)
   
-  skip <- NULL
-  if(any(!is.na(cov_info_subset$ranef_cov))){
-    stop(paste(cov_info_subset$covariate[!is.na(cov_info_subset$ranef_cov)], collapse = ", "), 
-            " has a random effect other than species. This is currently not supported.", call. = F)
-    skip <- c(skip, cov_info_subset$covariate[!is.na(cov_info_subset$ranef_cov)])
-  }
   
-  if(any(isTRUE(cov_info_subset$ranef_nested ))) {
-    stop(paste(cov_info_subset$covariate[isTRUE(cov_info_subset$ranef_nested )], collapse = ", "), 
-            " has a nested random effect. This is currently not supported.", call. = F)
-    skip <- c(skip, cov_info_subset$covariate[isTRUE(cov_info_subset$ranef_nested)])
-  }
+  # subset parameters of submodel
+  stopifnot(all(cov_info_subset$coef %in% object@params))
+  params_submodel <- object@params[grep(keyword_submodel, object@params)]
   
   
-  
-  
-  
-  
-  # subset posterior matrix
+  # subset posterior matrix to number of draws
   posterior_matrix <- as.matrix(mcmc.list)
   if(hasArg(draws)) {
-    posterior_matrix <- posterior_matrix[sample(1:nrow(posterior_matrix), draws),]
+    if(nrow(posterior_matrix) > draws)     posterior_matrix <- posterior_matrix[sample(1:nrow(posterior_matrix), draws),]
   } 
-  posterior_matrix <- posterior_matrix[,grep("^beta", colnames(posterior_matrix)), drop = FALSE]
+  
+  # subset posterior matrix to current submodel
+  posterior_matrix <- posterior_matrix[, grep(keyword_submodel, colnames(posterior_matrix))]
   
   if(nrow(posterior_matrix) > 1000) message("More than 1000 posterior samples. Watch RAM usage")
   
-  
-  # get covariate that beta parameters refer to
-  params_covariate <- sapply(strsplit(params, split = ".", fixed = TRUE), FUN = function (x) x[length(x)])
-  params_covariate <- params_covariate[params_covariate %in% names(object@data)]
-  
+  params_covariate <- cov_info_subset$covariate
+  if(length(params_covariate) == 0) stop ("No covariates found", call. = F)
   
   
   list_responses <- list()
+  
   
   # convert raster to covariate data frame
   if(class(x) == "RasterStack") {
     values_to_predict_all <- as.data.frame(raster::values(x))
   } else {
-    stop("x must be a data.frame")
+    if(is.data.frame(x)) {
+      values_to_predict_all <- x
+    } else {
+      stop("x must be a data.frame")
+    }
   }
-  # if(is.data.frame(x)){
-  #   #values_to_predict_all <- x
-  #   stop()
-  # }
+
   
-  
+  # identify cells with values
   index_not_na <- which(apply(values_to_predict_all, 1, FUN = function(x) all(!is.na(x))))
   
-  values_to_predict <- values_to_predict_all[index_not_na, , drop = F]
+  values_to_predict_subset <- values_to_predict_all[index_not_na, , drop = F]
   
   
   # create array for intercepts
-  array_NA <- array(data = NA, dim = c(nrow(values_to_predict),    # raster cell
-                                       object@data$M,    # species
-                                       nrow(posterior_matrix)))    # posterior sample
+  array_NA <- array(data = NA, dim = c(nrow(values_to_predict_subset),    # raster cell
+                                       object@data$M,                     # species
+                                       nrow(posterior_matrix)))           # posterior sample
   
   
-  # get intercepts
+  # # get intercepts
   out_beta0 <- array_NA
+  
   for(i in 1:dim(array_NA)[2]){    # species loop
-    out_beta0[,i,] <- posterior_matrix[, grep(paste0("beta0[", i, "]"), colnames(posterior_matrix), fixed = TRUE)]
+    if(!paste0(keyword_submodel_short, "0.mean") %in% object@params) {
+      # fixed intercept
+      out_beta0[,i,] <- posterior_matrix[, grepl(paste0(keyword_submodel_short, "0$"), colnames(posterior_matrix))] 
+    } else {
+      # random intercept
+      out_beta0[,i,] <- posterior_matrix[, colnames(posterior_matrix) %in% paste0(keyword_submodel_short, "0", "[", i, "]")] 
+    }
   }
   gc()
   
+  # check if this works
+  if(object.size(out_beta0) / 1e6 * (nrow(cov_info_subset) + 1) > 4000 ) message("Watch RAM usage. At least 4Gb will be required")
+  
+  
   out <- list()
-  # loop over covariates
-  for(covariate in 1:length(params_covariate)) {
-    out[[covariate]] <- array_NA
+  # loop over covariates 
+  for(cov in 1:nrow(cov_info_subset)) {
+    
+    current_cov  <- cov_info_subset$covariate[cov]
+    current_coef <- cov_info_subset$coef[cov]
+    
+    if(!is.na(cov_info_subset$ranef_cov[cov])){
+      stop(paste(current_cov, 
+                    " has a random effect other than species. This is currently not supported. Predictions would be wrong", call. = F))
+      next
+    }
+    
+    if(cov_info_subset$ranef_nested[cov])  {
+      stop(paste(current_cov, 
+                    " has a nested random effect. This is currently not supported. Predictions would be wrong", call. = F))
+      next
+    }
+    
+    out[[cov]] <- array_NA
+    
+    # determine data type of current covariate
+    covariate_is_numeric <- cov_info_subset$data_type [cov] == "cont"
+    covariate_is_factor  <- cov_info_subset$data_type [cov] == "categ"
     
     
-    # for continuous covariates (which have only a species index)
-    if(length(grep(paste0(params_covariate[covariate], "[", 1, "]"), colnames(posterior_matrix), fixed = TRUE)) == 1) {
-      for(i in 1:dim(out[[covariate]] )[2]){    # species loop
-        out[[covariate]][,i,] <-  sapply(posterior_matrix[, grep(paste0(params_covariate[covariate], "[", i, "]"), colnames(posterior_matrix), fixed = TRUE)], FUN = function(x){
-          x * values_to_predict [, params_covariate[covariate]]
+    covariate_is_fixed <- !cov_info_subset$ranef[cov]
+    covariate_is_ranef <- cov_info_subset$ranef[cov]
+    
+    covariate_is_site_cov <- ifelse(cov_info_subset$covariate_type [cov] == "siteCovs", T, F) 
+    
+    
+    # species loop
+    for(i in 1:dim(out[[cov]])[2]){
+      
+      if(covariate_is_numeric) {
+        
+        if(covariate_is_fixed)  index_covariate <- grep(paste0(current_coef, "$"), colnames(posterior_matrix))
+        if(covariate_is_ranef)  index_covariate <- grep(paste0(current_coef, "[", i, "]"), colnames(posterior_matrix), fixed = T)
+        
+        
+        out[[cov]][,i,] <-  sapply(posterior_matrix[, index_covariate], FUN = function(x){
+          x * values_to_predict_subset[, current_cov]
         })
       }
-    }
-    
-    # for categorical covariates (which have two indices: species and factor level - factor levels potentially differ by each cell)
-    # below expects parameters to be in order: species - factor level
-    
-    if(length(grep(paste0(params_covariate[covariate], "[", 1, "]"), colnames(posterior_matrix), fixed = TRUE)) == 0) {
       
-      unique_values <- unique(values_to_predict[, params_covariate[covariate]])
-      which_cell <- sapply(unique_values, FUN = function(x) which(values_to_predict[, params_covariate[covariate]] == x))
-      
-      for(factor_level in 1:length(unique_values)) {
+      if(covariate_is_factor) {
+
+        if(covariate_is_fixed) index_covariate <- grep(current_coef, colnames(posterior_matrix))
+        if(covariate_is_ranef) index_covariate <- grep(paste0(current_coef, "[", i, ","), colnames(posterior_matrix), fixed = T)
+
+        # this assumes that the numeric values in the raster correspond to the factor levels in the covariate
+        # since it uses the raster values to index the posterior matrix
         
-        for(i in 1:dim(out[[covariate]] )[2]){  # species loop
-          
-          # make matrix of posterior estimates
-          if(categ_order == "species,factor"){
-            matrix_tmp <- matrix(sapply(posterior_matrix[, grep(paste0(params_covariate[covariate], "[", i, ",", factor_level, "]"), colnames(posterior_matrix), fixed = TRUE)],
-                                        FUN = function(x){x}), ncol = dim(out[[covariate]])[3], nrow = length(which_cell[[factor_level]]), byrow = T)
-          }
-          if(categ_order == "factor,species"){
-            matrix_tmp <- matrix(sapply(posterior_matrix[, grep(paste0(params_covariate[covariate], "[", factor_level, ",", i, "]"), colnames(posterior_matrix), fixed = TRUE)],
-                                        FUN = function(x){x}), ncol = dim(out[[covariate]])[3], nrow = length(which_cell[[factor_level]]), byrow = T)
-          }
-          
-          out[[covariate]][which_cell[[factor_level]],i,] <-  matrix_tmp
-        }
+        # for data.frames, it seems to work with the categorical column being integer (= factor level, as in as.data.frame(rasterStack)), or proper factor
+        out[[cov]][,i,] <- posterior_matrix[, index_covariate[values_to_predict_subset[, current_cov]]]
+        
       }
-    }
-  }
+      suppressWarnings(rm(index_covariate))
+    }   # end species loop
+  }    # end covariate loop
   
   # sum up individual effects
   logit.psi <- Reduce('+', out) + out_beta0
@@ -159,21 +186,22 @@ predictionMapsCommunity <- function(object,
     
     # summarize estimates (across posterior samples)
     psi.mean <- apply(psi, MARGIN = c(1,2), mean)
-    psi.sd <- apply(psi, MARGIN = c(1,2), sd)
+    psi.sd   <- apply(psi, MARGIN = c(1,2), sd)
     
     # make data frame for ggplot
     psi.mean.melt <- reshape2::melt(psi.mean)
-    psi.sd.melt <- reshape2::melt(psi.sd)
+    psi.sd.melt   <- reshape2::melt(psi.sd)
     
     names(psi.mean.melt) <- c("cell_nr", "Species", "mean")
-    names(psi.sd.melt) <- c("cell_nr", "Species", "sd")
+    names(psi.sd.melt)   <- c("cell_nr", "Species", "sd")
     
     if(!is.null(dimnames(object@data$y)[[1]])) {
       psi.mean.melt$Species <- dimnames(object@data$y)[[1]][psi.mean.melt$Species]
-      psi.sd.melt$Species <- dimnames(object@data$y)[[1]][psi.sd.melt$Species]
+      psi.sd.melt$Species   <- dimnames(object@data$y)[[1]][psi.sd.melt$Species]
     }
     
-    #if(class(x) == "RasterStack") {
+    # fill rasters with predicted values
+    if(class(x) == "RasterStack") {
     raster_template <- raster::raster(x)
     r_pred_species <- r_pred_sd_species <- list()
     
@@ -181,15 +209,14 @@ predictionMapsCommunity <- function(object,
       r_pred_species[[i]]    <- raster_template
       r_pred_sd_species[[i]] <- raster_template
       
-      raster::values(r_pred_species[[i]]) [index_not_na] <- psi.mean.melt$mean[psi.mean.melt$Species == unique(psi.mean.melt$Species)[i]]
+      raster::values(r_pred_species[[i]]) [index_not_na]    <- psi.mean.melt$mean[psi.mean.melt$Species == unique(psi.mean.melt$Species)[i]]
       raster::values(r_pred_sd_species[[i]]) [index_not_na] <- psi.sd.melt$sd[psi.sd.melt$Species == unique(psi.sd.melt$Species)[i]]
     }
     names(r_pred_species) <- names(r_pred_sd_species) <- unique(psi.mean.melt$Species)
     
     stack_out_mean <- raster::stack(r_pred_species)
     stack_out_sd   <- raster::stack(r_pred_sd_species)
-    #}
-    
+    }
 
     
     if(interval == "confidence"){
@@ -207,6 +234,7 @@ predictionMapsCommunity <- function(object,
         psi.upper2$Species <- dimnames(object@data$y)[[1]][psi.upper2$Species]
       }
       
+      if(class(x) == "RasterStack") {
       raster_template <- raster::raster(x)
       r_pred_lower_species <- r_pred_upper_species <- list()
       
@@ -226,11 +254,24 @@ predictionMapsCommunity <- function(object,
                   sd    = stack_out_sd,
                   lower = stack_out_lower,
                   upper = stack_out_upper))
+      } else {
+        
+        return(data.frame(psi.mean.melt, 
+                          sd = psi.sd.melt$sd, 
+                          lower = psi.lower2[, 3], 
+                          upper = psi.upper2 [,3]))
+        
+      }
     }
     
     if(interval == "none"){
-      return(list(mean = stack_out_mean,
-                  sd = stack_out_sd))
+      if(class(x) == "RasterStack"){
+        return(list(mean = stack_out_mean,
+                    sd = psi.sd.melt))
+      } else {
+        return(data.frame(psi.mean.melt, 
+                          sd = psi.sd.melt$sd))
+      }
     }
   }
   
@@ -261,7 +302,7 @@ predictionMapsCommunity <- function(object,
     if(is.numeric(speciesSubset)) speciesIndex <- speciesSubset
     
     # species richness at each pixel for each posterior sample
-    psi.bin.sum <- apply(psi_bin_array[,speciesIndex,, drop = FALSE], MARGIN = c(1,3), sum)
+    psi.bin.sum <- apply(psi_bin_array[, speciesIndex,, drop = FALSE], MARGIN = c(1,3), sum)
     
     
     # Mean of richness estimate
@@ -274,28 +315,50 @@ predictionMapsCommunity <- function(object,
       psi.bin.sum.lower <- apply(psi.bin.sum, 1, quantile, ((1-level) / 2))
       psi.bin.sum.upper <- apply(psi.bin.sum, 1, quantile, (1 - (1-level) / 2))
     }
-    raster_template <- raster::raster(x)
     
-    r.psi.bin.sum.mean <- r.psi.bin.sum.sd <- r.psi.bin.sum.lower <- r.psi.bin.sum.upper <- raster_template
-    raster::values(r.psi.bin.sum.mean) [index_not_na] <- psi.bin.sum.mean
-    raster::values(r.psi.bin.sum.sd) [index_not_na] <- psi.bin.sum.sd
-    
-    if(interval == "none"){
-      stack_out <- raster::stack(list(mean = r.psi.bin.sum.mean,
-                                      sd = r.psi.bin.sum.sd))
-    }
-    
-    if(interval == "confidence"){
-      raster::values(r.psi.bin.sum.lower) [index_not_na] <- psi.bin.sum.lower
-      raster::values(r.psi.bin.sum.upper) [index_not_na] <- psi.bin.sum.upper
+    if(class(x) == "RasterStack") {
+      raster_template <- raster::raster(x)
       
-      stack_out <- raster::stack(list(mean  = r.psi.bin.sum.mean,
-                                      sd    = r.psi.bin.sum.sd,
-                                      lower = r.psi.bin.sum.lower,
-                                      upper = r.psi.bin.sum.upper))
+      r.psi.bin.sum.mean <- r.psi.bin.sum.sd <- r.psi.bin.sum.lower <- r.psi.bin.sum.upper <- raster_template
+      raster::values(r.psi.bin.sum.mean) [index_not_na] <- psi.bin.sum.mean
+      raster::values(r.psi.bin.sum.sd) [index_not_na] <- psi.bin.sum.sd
+      
+      if(interval == "none"){
+        stack_out <- raster::stack(list(mean = r.psi.bin.sum.mean,
+                                        sd = r.psi.bin.sum.sd))
+      }
+      
+      if(interval == "confidence"){
+        raster::values(r.psi.bin.sum.lower) [index_not_na] <- psi.bin.sum.lower
+        raster::values(r.psi.bin.sum.upper) [index_not_na] <- psi.bin.sum.upper
+        
+        stack_out <- raster::stack(list(mean  = r.psi.bin.sum.mean,
+                                        sd    = r.psi.bin.sum.sd,
+                                        lower = r.psi.bin.sum.lower,
+                                        upper = r.psi.bin.sum.upper))
+      }
+      
+      
+      return(stack_out)
+      
+    } else {
+      if(interval == "none"){
+        df_out <- data.frame(#x,
+                             mean = psi.bin.sum.mean,
+                             sd   = psi.bin.sum.sd)
+      }
+      
+      if(interval == "confidence"){
+        df_out <- data.frame(#x, 
+                             mean  = psi.bin.sum.mean,
+                             sd    = psi.bin.sum.sd, 
+                             lower = psi.bin.sum.lower,
+                             upper = psi.bin.sum.upper)
+      }
+      
+      return(df_out)
+      
     }
-    
-    return(stack_out)
   }
 }
 
@@ -311,13 +374,14 @@ predictionMapsCommunity <- function(object,
 #' @param type character. "psi" for species occupancy estimates, "richness" for species richness estimates
 #' @param draws  Number of draws from the posterior to use when generating the plots. If fewer than draws are available, they are all used
 #' @param level  Probability mass to include in the uncertainty interval
-#' @param interval    # Type of interval calculation. Can be "none" or "confidence". Can be slow for type = psi with many cells and posterior samples. Can be abbreviated.
-#' @param x   raster stack. Must be scaled with same parameters as site covariates used in model, and have same names
+#' @param interval    # Type of interval calculation. Can be "none" or "confidence". Can be slow for type = "psi" with many cells and posterior samples. Can be abbreviated.
+#' @param x   raster stack or data.frame. Must be scaled with same parameters as site covariates used in model, and have same names. 
 #' @param speciesSubset  species to include in richness estimates. Can be index number or species names.
-#' @param categ_order a temporary argument. Ignore
 #'
-#' @return A raster stack
+#' @return A raster stack or data.frame, depending on x
+#' @details Categorical covariates in
 #' @importFrom  stats rbinom sd
+#' @importFrom utils object.size
 #' @export
 #'
 
