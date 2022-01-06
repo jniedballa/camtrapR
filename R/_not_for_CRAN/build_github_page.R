e43b8dcc72901d3c1a164f9ffe3c54f83da64f67
Jürgen Niedballa <camtrapr@gmail.com>
2022-01-06 18:23:07 +0100

    added code for knitting vignette 5

--- R/_not_for_CRAN/build_github_page.R
+++ R/_not_for_CRAN/build_github_page.R
@@ -2,11 +2,29 @@ library(pkgdown)
 require(devtools)
 
 
+Sys.setlocale("LC_ALL","English")
+
+
 build_home()
 #pkgdown::build_site()   # build whole github page
 
 build_news()       # NEWS section
 build_articles()   # all vignettes 
  build_article(name = "camtrapr1")   # is for individual vignettes
-# build_article(name = "camtrapr3")   # is for individual vignettes
+build_article(name = "camtrapr5")   # is for individual vignettes
+ build_articles_index()
 build_reference()  # function documentation
+
+
+
+
+library(rmarkdown)
+
+wd <- "C:/Users/niedballa/Documents/GitHub/camtrapR/vignettes"
+render(input = file.path(wd, "camtrapr5.Rmd"),
+     #output = file.path(wd, "camtrapR5.html")
+     output_format = "html_document",
+     output_options = list(#self_contained = T,
+                           toc = T,
+                           toc_depth = 2,
+                           toc_float = T))
