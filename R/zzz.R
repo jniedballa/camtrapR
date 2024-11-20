5a25f9372ac686bf3675f3b2a13647d15893fba6
Jürgen Niedballa <camtrapr@gmail.com>
2024-11-20 14:51:53 +0800

    add more objects

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,12 @@
 globalVariables(c(
   "PAO", "Predicted", "Records", "Species", "Stations",
-  "covariateValue", "lower", "upper", "median"
+  "covariateValue", "lower", "upper", "median",
+  "modelEffects",
+  "generateFormula",
+  "export_table",
+  "plot_effect_det",
+  "plot_effect_occu",
+  "generate_spatial_predictions"
+  
+  
 ))
