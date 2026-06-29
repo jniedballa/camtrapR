a402f026c36b1842e95458b340fe2ab33c6cc411
Juergen <camtrapr@gmail.com>
2026-06-29 13:13:51 +0800

    if placename missing, use "deployment_id" as fallback with warning. Error if all timestamps failed to parse

--- R/readWildlifeInsights.R
+++ R/readWildlifeInsights.R
@@ -41,7 +41,9 @@ readWildlifeInsights <- function(directory = NULL,
   }
   
   # Define required columns for validation
-  required_deployment_cols <- c("deployment_id", "placename", "camera_id", "start_date", "end_date")
+  required_deployment_cols <- c("deployment_id", 
+                                # "placename",   # expected by metadata standard, but not included in many downloads. Handle separately.
+                                "camera_id", "start_date", "end_date")
   required_image_cols <- c("deployment_id", "timestamp")
   
   # Load data from ZIP file
@@ -165,6 +167,10 @@ readWildlifeInsights <- function(directory = NULL,
   
   # Check for required columns in deployments
   missing_deployment_cols <- required_deployment_cols[!required_deployment_cols %in% colnames(deployments)]
+  # TODO: some WildlifeInsights datasets lack "placenames" column, leading to error here. 
+  # Unclear why they are missing, since they are part of metadata standard. 
+  # Reported as bug
+  # maybe use deployment_id as a fallback?
   if (length(missing_deployment_cols) > 0) {
     stop(paste("Missing required columns in deployments data:", 
                paste(missing_deployment_cols, collapse = ", "),
@@ -188,6 +194,12 @@ readWildlifeInsights <- function(directory = NULL,
                     "mdy HMS",  
                     "dmy HMS"            
   )
+  
+  if(!stationCol %in% colnames(deployments)) {
+    warning("deployments.csv does not contain a 'placename' column. Using 'deployment_id' as a fallback.", call. = F)
+    if(any(duplicated(deployments[, c(deploymentCol, cameraCol)]))) stop("Cannot use deployment_id as fallback due to duplicated deployment_id / camera_id.")
+    stationCol <- deploymentCol
+  }
   
   # Check for duplicate deployment IDs
   if(any(duplicated(deployments[, deploymentCol]))) {
@@ -380,6 +392,12 @@ readWildlifeInsights <- function(directory = NULL,
   # Ensure record date/time column is formatted correctly
   record_datetime_format <- c("ymd HMS",    # standard format
                               "mdy HM")     # if user modified content of zip in Excel
+  # NOTE: Some WI downloads have unexpected timestamp formats, e.g. "Fri Jul 18 2025 05:17:21 GMT+0000 (Coordinated Universal Time)", 
+  # should by "yyyy-mm-dd hh:mm:ss"
+  # Reported as bug. 
+  # Don't parse directly here. Error out.
+  
+  
   
   # Validate timestamp column
   if (!is.character(images_merged$timestamp)) {
@@ -399,6 +417,7 @@ readWildlifeInsights <- function(directory = NULL,
       "Supported formats are: ", paste(record_datetime_format, collapse = ", "), ".\n",
       "These records will have NA timestamps in the output."
     ))
+    if(all(is.na(timestamps_parsed))) stop("All timestamps failed to parse.", call. = FALSE)
   }
   
   images_merged$timestamp <- as.character(timestamps_parsed)
