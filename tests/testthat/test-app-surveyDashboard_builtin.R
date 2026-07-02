796fa3d103b0ea2777771a85606f7d8d3654478e
Juergen <camtrapr@gmail.com>
2026-07-03 00:45:17 +0800

    add cleanup code so RAM doesn't fill up

--- tests/testthat/test-app-surveyDashboard_builtin.R
+++ tests/testthat/test-app-surveyDashboard_builtin.R
@@ -4,10 +4,29 @@ load_timeout <- 30 * 1000  # 20 seconds
 
 
 
+
+test_that("{shinytest2} recording: surveyDashboard_builtin_launch", {
+  local_app_support(test_path("apps/surveyDashboard_builtin"))
+  app <- AppDriver$new(test_path("apps/surveyDashboard_builtin"), name = "surveyDashboard_builtin_launch",
+      height = 993, width = 1619, load_timeout = load_timeout)
+  
+  # Ensure the R session closes when the test ends
+  withr::defer(app$stop()) 
+  
+  # if reusing the same app across tests, use 
+  # withr::defer(app$stop(), testthat::teardown_env())  once (kills app when test suite ends)
+  
+  app$expect_values()
+})
+
+
 test_that("{shinytest2} recording: surveyDashboard_builtin_filters", {
   local_app_support(test_path("apps/surveyDashboard_builtin"))
   app <- AppDriver$new(test_path("apps/surveyDashboard_builtin"), name = "surveyDashboard_builtin_filters",
                        height = 993, width = 1619, load_timeout = load_timeout)
+  
+  withr::defer(app$stop()) 
+  
   app$set_inputs(sidebarItemExpanded = "DataFilters")
   app$set_inputs(categoryValues = c("StationB", "StationC"))
   app$click("applyFilter_camtrap_properties")
@@ -19,18 +38,13 @@ test_that("{shinytest2} recording: surveyDashboard_builtin_filters", {
 })
 
 
-test_that("{shinytest2} recording: surveyDashboard_builtin_launch", {
-  local_app_support(test_path("apps/surveyDashboard_builtin"))
-  app <- AppDriver$new(test_path("apps/surveyDashboard_builtin"), name = "surveyDashboard_builtin_launch",
-      height = 993, width = 1619, load_timeout = load_timeout)
-  app$expect_values()
-})
-
-
 test_that("{shinytest2} recording: surveyDashboard_builtin_correlations", {
   local_app_support(test_path("apps/surveyDashboard_builtin"))
   app <- AppDriver$new(test_path("apps/surveyDashboard_builtin"), name = "surveyDashboard_builtin_correlations",
       height = 993, width = 1619, load_timeout = 30000)
+
+  withr::defer(app$stop()) 
+  
   app$set_inputs(sidebarItemExpanded = "DataProcessing")
   app$set_inputs(correlationMethod = "spearman")
   app$set_inputs(correlationMethod = "kendall")
@@ -55,10 +69,12 @@ test_that("{shinytest2} recording: surveyDashboard_builtin_detectionHistory", {
   local_app_support(test_path("apps/surveyDashboard_builtin"))
   app <- AppDriver$new(test_path("apps/surveyDashboard_builtin"), name = "surveyDashboard_builtin_detectionHistory",
       height = 993, width = 1619, load_timeout = 30000)
+  
+  withr::defer(app$stop()) 
+  
   app$set_inputs(sidebarItemExpanded = "Single-speciesOccupancy")
   app$set_inputs(species_dethist = "MNE")
   app$set_inputs(day1_single_species = "station")
-  app$set_inputs(occasionLength_single_species = 6)
   app$set_inputs(occasionLength_single_species = 5)
   app$expect_values()
 })
@@ -69,6 +85,10 @@ test_that("{shinytest2} recording: surveyDashboard_builtin_occupancy", {
   local_app_support(test_path("apps/surveyDashboard_builtin"))
   app <- AppDriver$new(test_path("apps/surveyDashboard_builtin"), name = "surveyDashboard_builtin_occupancy",
       height = 993, width = 1619, load_timeout = 30000)
+  
+  withr::defer(app$stop()) 
+  
+  
   app$set_inputs(sidebarItemExpanded = "Single-speciesOccupancy")
   app$click("basic_run_model")
   app$click("basic_add_to_modsel")
@@ -89,8 +109,11 @@ test_that("{shinytest2} recording: surveyDashboard_builtin_occupancy", {
 
 test_that("{shinytest2} recording: surveyDashboard_builtin_communiy_UI", {
   local_app_support(test_path("apps/surveyDashboard_builtin"))
-  app <- AppDriver$new(test_path("apps/surveyDashboard_builtin"), name = "surveyDashboard_builtin_communiy_UI", 
+  app <- AppDriver$new(test_path("apps/surveyDashboard_builtin"), name = "surveyDashboard_builtin_communiy_UI",
       height = 993, width = 1619, load_timeout = 30000)
+  
+  withr::defer(app$stop()) 
+  
   app$set_inputs(detCovFixed = "utm_y")
   app$set_inputs(occuCovRanef = "utm_x")
   app$set_inputs(detCovIndep = "utm_y")
