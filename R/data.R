5bee21cdc6a6cb1e4db620859f331d129a591b61
Alexandre Courtiol <alexandre.courtiol@gmail.com>
2026-07-09 13:45:27 +0200

    Correct column number

--- R/data.R
+++ R/data.R
@@ -5,22 +5,23 @@
 #'
 #' The variables are as follows:
 #' \tabular{ll}{
-#' \code{Station} \tab Camera trap station ID \cr
-#' \code{Species} \tab Species ID \cr
-#' \code{DateTimeOriginal} \tab Date and time as extracted from image \cr
+#' \code{Station} \tab camera trap station ID \cr
+#' \code{Species} \tab species ID \cr
+#' \code{DateTimeOriginal} \tab date and time as extracted from image \cr
 #' \code{Date} \tab record date \cr
 #' \code{Time} \tab record time of day \cr
 #' \code{delta.time.secs} \tab time difference to first species record at a station (seconds) \cr
 #' \code{delta.time.mins} \tab time difference to first species record at a station (minutes) \cr
 #' \code{delta.time.hours} \tab time difference to first species record at a station (hours) \cr
 #' \code{delta.time.days} \tab time difference to first species record at a station (days) \cr
-#' \code{Directory} \tab Image directory \cr
+#' \code{Directory} \tab image directory \cr
 #' \code{FileName} \tab image filename \cr
+#' \code{n_images} \tab the number of images \cr
 #' }
 #'
 #' @docType data
 #'
-#' @format A data frame with 39 rows and 11 variables
+#' @format A data frame with 39 rows and 12 variables
 #'
 #' @usage data(recordTableSample)
 #'
