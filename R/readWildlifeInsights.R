359308b7600935cf9e9c62294e5beb8c48697c15
Juergen <camtrapr@gmail.com>
2026-07-26 09:41:42 +0200

    improve formatting of values section

--- R/readWildlifeInsights.R
+++ R/readWildlifeInsights.R
@@ -10,17 +10,20 @@
 #' @param image_file character. Path to the images CSV file.
 #'
 #' @return A list containing three elements:
-#' \describe{
-#'   \item{CTtable}{The full camera trap table, based on deployments.csv}
-#'   \item{CTtable_aggregated}{An aggregated version of the camera trap table, with one row per station}
-#'   \item{recordTable}{The record table, based on images.csv with additional columns from deployments.csv}
+#' \itemize{
+#'   \item \strong{CTtable}: The full camera trap table, based on deployments.csv
+#'   \item \strong{CTtable_aggregated}: An aggregated version of the camera trap table, with one row per station
+#'   \item \strong{recordTable}: The record table, based on images.csv with additional columns from deployments.csv
 #' }
+#' 
+#' 
+#' 
 #' @export
 #'
 #' @examples
 #' \dontrun{
 #' # Reading from a directory
-#' wi_data <- readWildlifeInsights(directory = "path/to/csv/files")
+#' wi_data <- readWildlifeInsights(directory = "path/to/csv_files")
 #'
 #' # Reading from a ZIP file
 #' wi_data <- readWildlifeInsights(zipfile = "path/to/wildlife_insights_export.zip")
@@ -167,10 +170,7 @@ readWildlifeInsights <- function(directory = NULL,
   
   # Check for required columns in deployments
   missing_deployment_cols <- required_deployment_cols[!required_deployment_cols %in% colnames(deployments)]
-  # TODO: some WildlifeInsights datasets lack "placenames" column, leading to error here. 
-  # Unclear why they are missing, since they are part of metadata standard. 
-  # Reported as bug
-  # maybe use deployment_id as a fallback?
+  
   if (length(missing_deployment_cols) > 0) {
     stop(paste("Missing required columns in deployments data:", 
                paste(missing_deployment_cols, collapse = ", "),
@@ -195,6 +195,7 @@ readWildlifeInsights <- function(directory = NULL,
                     "dmy HMS"            
   )
   
+  # TODO: This was necessitated by a bug in previous WI exports. Fixed in 7/2026. Should not be necessary anymore. Remove once WI exports confirmed to match metadata standards.
   if(!stationCol %in% colnames(deployments)) {
     warning("deployments.csv does not contain a 'placename' column. Using 'deployment_id' as a fallback.", call. = F)
     if(any(duplicated(deployments[, c(deploymentCol, cameraCol)]))) stop("Cannot use deployment_id as fallback due to duplicated deployment_id / camera_id.")
