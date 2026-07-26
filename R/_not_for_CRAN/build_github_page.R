c6a6e4867509f9239b07e2d0c2001d8df4071505
Juergen <camtrapr@gmail.com>
2026-07-26 09:42:04 +0200

    remove vignette creation block

--- R/_not_for_CRAN/build_github_page.R
+++ R/_not_for_CRAN/build_github_page.R
@@ -23,26 +23,3 @@ build_reference()  # function documentation
 build_reference_index(pkg = ".")
 
 
-
-library(rmarkdown)
-
-# building html vignette with code below ensures that toc works correctly
-wd <- here::here("vignettes")
-
-render(input = file.path(wd, "camtrapr5.Rmd"),
-     #output = file.path(wd, "camtrapR5.html")
-     output_format = "html_document",
-     output_options = list(#self_contained = T,
-                           toc = T,
-                           toc_depth = 2,
-                           toc_float = T))
-
-
-render(input = file.path(wd, "camtrapr6.Rmd"),
-       #output = file.path(wd, "camtrapR5.html")
-       output_format = "html_document",
-       output_options = list(#self_contained = T,
-         toc = T,
-         toc_depth = 2,
-         toc_float = T))
-
